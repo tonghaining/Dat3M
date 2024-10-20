1b1abe4a0b5e6b0d53c4adaa6d423be84f16ac1c
haining <haining.tong@helsinki.fi>
2024-10-20 21:53:17 +0300

    add opencl spirv compilation visitor

--- dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/Tag.java
@@ -381,6 +381,7 @@ public final class Tag {
         // Space
         public static final String GLOBAL_SPACE = "GLOBAL";
         public static final String LOCAL_SPACE = "LOCAL";
+        public static final String GENERIC_SPACE = "GENERIC";
         // Barrier
         public static final String ENTRY_FENCE = "EF";
         public static final String EXIT_FENCE = "XF";
