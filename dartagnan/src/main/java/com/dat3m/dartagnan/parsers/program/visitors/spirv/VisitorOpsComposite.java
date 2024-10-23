8565af27d7867f876b47edb1f1fa7dc1ff80acc9
Haining <haining.tong@helsinki.fi>
2024-10-23 14:44:14 +0300

    composite extract

--- dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorOpsComposite.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/parsers/program/visitors/spirv/VisitorOpsComposite.java
@@ -4,29 +4,22 @@ import com.dat3m.dartagnan.exception.ParsingException;
 import com.dat3m.dartagnan.expression.Expression;
 import com.dat3m.dartagnan.expression.ExpressionFactory;
 import com.dat3m.dartagnan.expression.Type;
-import com.dat3m.dartagnan.expression.integers.IntLiteral;
 import com.dat3m.dartagnan.expression.type.AggregateType;
 import com.dat3m.dartagnan.expression.type.ArrayType;
-import com.dat3m.dartagnan.expression.type.BooleanType;
-import com.dat3m.dartagnan.expression.type.IntegerType;
 import com.dat3m.dartagnan.parsers.SpirvBaseVisitor;
 import com.dat3m.dartagnan.parsers.SpirvParser;
 import com.dat3m.dartagnan.parsers.program.visitors.spirv.builders.ProgramBuilder;
-import com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.BuiltIn;
-import com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.SpecId;
 import com.dat3m.dartagnan.program.Register;
+import com.dat3m.dartagnan.program.event.Event;
+import com.dat3m.dartagnan.program.event.EventFactory;
+import com.dat3m.dartagnan.program.event.core.Local;
 import org.antlr.v4.runtime.RuleContext;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.DecorationType.BUILT_IN;
-import static com.dat3m.dartagnan.parsers.program.visitors.spirv.decorations.DecorationType.SPEC_ID;
-
-public class VisitorOpsComposite extends SpirvBaseVisitor<Expression> {
+public class VisitorOpsComposite extends SpirvBaseVisitor<Event> {
 
     private static final ExpressionFactory expressions = ExpressionFactory.getInstance();
 
@@ -37,17 +30,21 @@ public class VisitorOpsComposite extends SpirvBaseVisitor<Expression> {
     }
 
     @Override
-    public Expression visitOpCompositeExtract(SpirvParser.OpCompositeExtractContext ctx) {
+    public Event visitOpCompositeExtract(SpirvParser.OpCompositeExtractContext ctx) {
         String id = ctx.idResult().getText();
-        Type type = builder.getType(ctx.idResultType().getText());
+        String typeId = ctx.idResultType().getText();
         String compositeId = ctx.composite().getText();
         Expression composite = builder.getExpression(compositeId);
         int index = Integer.parseInt(ctx.indexesLiteralInteger().stream().map(RuleContext::getText)
                 .collect(Collectors.joining()));
-        return builder.addExpression(id, makeCompositeElement(composite, index, type));
+        Expression result = extractCompositeElement(composite, index, typeId);
+        Register register = builder.addRegister(id, typeId);
+        Local event = EventFactory.newLocal(register, result);
+        return builder.addEvent(event);
     }
 
-    private Expression makeCompositeElement(Expression composite, int index, Type type) {
+    private Expression extractCompositeElement(Expression composite, int index, String typeId) {
+        Type type = builder.getType(typeId);
         if (composite.getType() instanceof AggregateType aType) {
             List<Type> elementTypes = aType.getDirectFields();
             if (index >= elementTypes.size()) {
