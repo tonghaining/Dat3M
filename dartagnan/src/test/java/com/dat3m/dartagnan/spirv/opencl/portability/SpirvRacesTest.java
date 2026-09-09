ed6e033e4930daf815fda65df14fcc423c788f54
Haining <haining.tong@helsinki.fi>
2026-09-09 21:15:11 +0300

    add portability spvasm tests

---
+++ dartagnan/src/test/java/com/dat3m/dartagnan/spirv/opencl/portability/SpirvRacesTest.java
@@ -0,0 +1,61 @@
+package com.dat3m.dartagnan.spirv.opencl.portability;
+
+import com.dat3m.dartagnan.configuration.Arch;
+import com.dat3m.dartagnan.configuration.Method;
+import com.dat3m.dartagnan.parsers.cat.ParserCat;
+import com.dat3m.dartagnan.parsers.program.ProgramParser;
+import com.dat3m.dartagnan.program.Program;
+import com.dat3m.dartagnan.utils.Result;
+import com.dat3m.dartagnan.utils.TestHelper;
+import com.dat3m.dartagnan.verification.VerificationTask;
+import com.dat3m.dartagnan.wmm.Wmm;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.EnumSet;
+
+import static com.dat3m.dartagnan.configuration.Property.CAT_SPEC;
+import static com.dat3m.dartagnan.utils.ResourceHelper.getRootPath;
+import static com.dat3m.dartagnan.utils.ResourceHelper.getTestResourcePath;
+import static com.dat3m.dartagnan.utils.Result.PASS;
+import static com.dat3m.dartagnan.utils.Result.FAIL;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class SpirvRacesTest {
+
+    private final String modelPath = getRootPath("cat/opencl.cat");
+    private final String programPath;
+    private final Result expected;
+
+    public SpirvRacesTest(String file, Result expected) {
+        this.programPath = getTestResourcePath("spirv/opencl/portability/" + file);
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}: {0}, {1}")
+    public static Iterable<Object[]> data() throws IOException {
+        return Arrays.asList(new Object[][]{
+                {"mp-cross-region.spvasm", FAIL},
+                {"mp-cross-region-fix.spvasm", PASS}
+        });
+    }
+
+    @Test
+    public void test() throws Exception {
+        assertEquals(expected, TestHelper.createAndRunSolver(mkTask(), Method.EAGER));
+    }
+
+    private VerificationTask mkTask() throws Exception {
+        VerificationTask.VerificationTaskBuilder builder = VerificationTask.builder()
+                .withConfig(TestHelper.getBasicConfig())
+                .withTarget(Arch.OPENCL);
+        Program program = new ProgramParser().parse(new File(programPath));
+        Wmm mcm = new ParserCat().parse(new File(modelPath));
+        return builder.build(program, mcm, EnumSet.of(CAT_SPEC));
+    }
+}
